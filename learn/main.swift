a15b1201b841dc5fbc51476ca411dc0a623c332d
20161104572 <a20161104572@mac28.local>
2018-09-07 16:21:30 +0800

    Initial Commit

---
+++ learn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  learn
+//
+//  Created by 20161104572 on 2018/9/7.
+//  Copyright © 2018年 20161104572. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
