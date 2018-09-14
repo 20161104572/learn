6ec5b40808d58828fd397a492f5840a93255c393
20161104572 <2606612104@qq.com>
2018-09-14 16:45:11 +0800

    x+y
    Good

---
+++ learnthree/learnthree/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  learnthree
+//
+//  Created by 20161104572 on 2018/9/14.
+//  Copyright © 2018年 20161104572. All rights reserved.
+//
+
+import Foundation
+
+
+var x = 3
+var y = 4
+let z = x+y
+print(z)
+
